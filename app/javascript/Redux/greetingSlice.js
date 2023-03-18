b359b623fabf28e73a98e02e82feaa9d450d3cdb
Ernest Anyewe Adonu <ernestadonu@yahoo.com>
2023-03-18 21:39:04 +0000

    Update greetingSlice.js

--- app/javascript/Redux/greetingSlice.js
+++ app/javascript/Redux/greetingSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const fetchGreetings = createAsyncThunk(
   "greetings/fetchGreetings",
   async () => {
-    const response = await fetch("http://127.0.0.1:3000/api/v1/users/1/greetings");
+    const response = await fetch("http://127.0.0.1:3000/api/v1/users/1/greetings/");
     const data = await response.json();
     return data;
   }
